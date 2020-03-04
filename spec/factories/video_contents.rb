2745d4dded3a4b425528ef62ba6b85be0222e74d
Umesh <umesh@gurzu.com>
2020-03-04 22:30:15 +0545

    Development:  Added API for listing movies and make the fixture pass.

--- spec/factories/video_contents.rb
+++ spec/factories/video_contents.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-	factory :video_content_movies do |f|
+	factory :video_content_movie, class: VideoContent do |f|
 		f.title { Faker::Name.name }
 		f.plot { 1 }
 		f.type {"Movie"}
 	end
 
-	factory :video_content_seasons do |f|
+	factory :video_content_season, class: VideoContent do |f|
 		f.title {Faker::Name.name}
 		f.plot {2}
 		f.type {"Season"}
