879673c82548a9d48205c1e1077270cb9a756b39
Umesh <umesh@gurzu.com>
2020-03-11 10:45:20 +0545

    Development: added test for the purchase create action.

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id               :bigint           not null, primary key
+#  price            :decimal(3, 2)    default("2.99")
+#  video_quality    :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :bigint           not null
+#  video_content_id :bigint           not null
+#
+# Indexes
+#
+#  index_purchases_on_user_id           (user_id)
+#  index_purchases_on_video_content_id  (video_content_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#  fk_rails_...  (video_content_id => video_contents.id)
+#
 FactoryBot.define do
 	factory :valid_purchase, class: Purchase do |f|
 		f.price {2.99}
