9e936f504fa623f0cb6cc67168bdc6c35884ca20
Umesh <umesh@gurzu.com>
2020-03-27 21:17:02 +0545

    Development: Created a vouchers table to store coupons.

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: vouchers
+#
+#  id             :bigint           not null, primary key
+#  voucher_number :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Voucher < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_152226) do
+ActiveRecord::Schema.define(version: 2020_03_27_152914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 2020_03_27_152226) do
     t.integer "plot"
     t.integer "number"
     t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "vouchers", force: :cascade do |t|
+    t.string "voucher_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
